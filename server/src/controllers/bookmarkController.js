bad6b354c2f3d3459ac0a314304bc11ef592d176
James <didierlaffin@gmail.com>
2026-07-04 09:46:24 +0100

    Milestone 3 : Express API connected to PostGreSQL with Prisma

--- server/src/controllers/bookmarkController.js
+++ server/src/controllers/bookmarkController.js
@@ -14,6 +14,27 @@ async function getBookmarks(req, res) {
   }
 }
 
+
+async function createBookmark(req, res) {
+  try {
+    const bookmark = await service.createBookmark(req.body);
+
+    res.status(201).json(bookmark);
+
+  } catch (error) {
+  console.error(error);
+
+  res.status(500).json({
+    error: "Internal Server Error",
+  });
+}
+
+
+
+
+
+}
+
 module.exports = {
-  getBookmarks,
+  getBookmarks,createBookmark,
 };
