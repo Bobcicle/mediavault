14f74e33fe67f658d116b1ead60da25edd521bc4
James <didierlaffin@gmail.com>
2026-07-05 19:59:12 +0100

    Implement complete CRUD API and frontend create/delete

--- server/src/controllers/bookmarkController.js
+++ server/src/controllers/bookmarkController.js
@@ -29,12 +29,46 @@ async function createBookmark(req, res) {
   });
 }
 
+}
 
+async function deleteBookmark(req, res) {
+  try {
+    await service.deleteBookmark(Number(req.params.id));
 
+    res.sendStatus(204);
 
+  } catch (error) {
+    console.error(error);
 
+    res.status(500).json({
+      error: "Internal Server Error",
+    });
+  }
+}
+
+async function updateBookmark(req, res) {
+  try {
+
+console.log("Params:", req.params);
+console.log("Body:", req.body);
+    const bookmark = await service.updateBookmark(
+      Number(req.params.id),
+      req.body
+    );
+
+    res.json(bookmark);
+
+  } catch (error) {
+  console.error(error);
+
+  res.status(500).json({
+    message: error.message,
+    code: error.code,
+    meta: error.meta,
+  });
+}
 }
 
 module.exports = {
-  getBookmarks,createBookmark,
+  getBookmarks,createBookmark,deleteBookmark,updateBookmark,
 };
